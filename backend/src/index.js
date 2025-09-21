fcd45e257a27f35a68653416944d25d24f590b79
Sonalee <spiratualworld21@gmail.com>
2025-09-21 20:10:44 +0530

    my first chat apllication

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js"
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import { app, server } from "./lib/socket.js"
+import path from "path"
+
+dotenv.config()
+const PORT = process.env.PORT
+const __dirname = path.resolve()
+
+app.use(express.json())             //allow json data out of body
+app.use(cookieParser())             //allow you to parse the cookie
+app.use(cors({                      //allow authentication headers 
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "..frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "..frontend", "dist", "index.html"))
+    })
+}
+
+server.listen(PORT, () => {
+    console.log('Server is running on PORT ' + PORT);
+    connectDB()
+})
